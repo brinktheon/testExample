df08ff46a661c0639a6e9082e032b4d282271a3d
chris <andreew.gl@yandex.ru>
2018-04-12 14:59:04 +0300

    qwe

--- Задания по SQL/Задание 8/Query 8.sql
+++ Задания по SQL/Задание 8/Query 8.sql
@@ -1,11 +1,14 @@
 use Warehouse;
 /*Задание 8*/
 select m.First_Name as 'Имя', m.Last_Name 'Фамилия'
-from Managers m 		
-	Where 1 < (	
-				select Count(ws.Store_Id)
-				from Warehouse_Store ws 					
-				inner join Warehouse_Manager wm 
-						on ws.Warehouse_Id = wm.Warehouse_Id AND
-						wm.Manager_Id = m.id
-			)
+from Managers m 
+inner join (	
+				select wm.Manager_Id
+				from Warehouse_Manager wm 					
+				inner join Warehouse_Store ws 
+						on ws.Warehouse_Id = wm.Warehouse_Id
+				Group by wm.Manager_Id
+				Having  Count(ws.Store_Id) > 1
+
+			) as inj on m.id = inj.Manager_Id;
+				
--- Задания по SQL/Задание 9/Query 9.sql
+++ Задания по SQL/Задание 9/Query 9.sql
@@ -1,10 +1,13 @@
 use Warehouse;
 /*Задание 9*/
-select top 3 m.First_Name as 'Имя', m.Last_Name as 'Фамилия',
-			 (
-					select Sum(wp.Quantity_Product) as 'Кол-во товара на складах'
+select m.First_Name as 'Имя', m.Last_Name as 'Фамилия'
+from  Managers m
+inner join
+			(
+			select top 3 wm.Manager_Id
 					from Warehouse_Manager wm
 					inner join Warehouse_Products wp 
-						on wp.Warehouse_Id = wm.Warehouse_Id AND wm.Manager_Id = m.id
-			  )
-from Managers m;
+						on wp.Warehouse_Id = wm.Warehouse_Id
+			Group by wm.Manager_Id
+			Order by Sum(wp.Quantity_Product) Desc
+			) as inj on inj.Manager_Id = m.id;
